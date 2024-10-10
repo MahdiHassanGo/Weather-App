a37c1b1d9af896da07cae84283cc1c4f45bf8ae2
MahdiHassanGo <contact.mahdiasif@gmail.com>
2024-10-10 11:54:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const container = document.getElementById('container');
+const search = document.getElementById('search-box');
+const weatherBox = document.getElementById('weather-box');
+const weatherDetails = document.getElementById('weather-details');
+
+
+search.addEventListener('click', () =>{
+    const APIkey= "0051d34aae165f7b85c25157dcd89368";
+    const city = document.getElementById('')
+})
